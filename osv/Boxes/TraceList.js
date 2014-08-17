8a4d01c76cb7eddbb31691c55bca0e79542bb592
Daniel Zautner <dzautner@gmail.com>
2014-08-17 11:48:34 +0300

    added filtration method for possible tracepoints

--- osv/Boxes/TraceList.js
+++ osv/Boxes/TraceList.js
@@ -4,15 +4,27 @@ OSv.Boxes = OSv.Boxes || {};
 OSv.Boxes.TraceList = (function() {
 
   function TraceList() {
+    window.tl = this;
   }
+
 
   TraceList.prototype = new OSv.Boxes.StaticBox();
 
   TraceList.prototype.template = "/osv/templates/boxes/TraceList.html";
 
+  TraceList.prototype.filter = function(keyword) {
+    $('[data-tracepoint-name]').each(function () {
+      var $trace = $(this);
+      if ($trace.html().indexOf(keyword) == -1) {
+        $trace.hide();
+      } else {
+        $trace.show();
+      }
+    })
+  };
+  
   TraceList.prototype.fetchData = function () {
     return OSv.API.Trace.all(function (args) {
-      console.log(args);
       return args;
     })
   };
