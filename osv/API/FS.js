98416bf5cf512ab4ab48f483ef80c06e0ce8a104
Lord Daniel Zautner <dzautner@gmail.com>
2014-08-25 10:54:16 +0300

    added method to get percentage of free disk space

--- osv/API/FS.js
+++ osv/API/FS.js
@@ -3,7 +3,19 @@ OSv.API = OSv.API || {};
 
 OSv.API.FS = (function() {
 
+  var apiGETCall = helpers.apiGETCall, 
+    df = apiGETCall("/fs/df/"),
+    free;
+
+
+  free = function() {
+    return df().then(function (res) {
+      return (res[0].bfree / res[0].btotal) * 100;
+    })
+  };
+
   return { 
+    free: free
   };
 
 }());
