8c432f7e4c13f993bcc3b3ad06637c999d10ad4e
Daniel Zautner <dzautner@gmail.com>
2014-08-26 11:29:38 +0300

    added fetch data method to threadstimeline box

--- osv/Boxes/ThreadsTimeline.js
+++ osv/Boxes/ThreadsTimeline.js
@@ -10,6 +10,24 @@ OSv.Boxes.ThreadsTimeline = (function() {
 
   ThreadsTimeline.prototype.template = "/osv/templates/boxes/EmptyBox.html";
 
+
+  ThreadsTimeline.prototype.fetchData = function () {
+    return OSv.API.OS.threadsGraph().then(function (res) {
+      return $.map(res, function(thread) {
+        return thread;
+      }).filter(function (thread) {
+        return ids.indexOf( thread.id ) != -1;
+      }).map(function (thread) {
+        thread.statusTimeline = thread.statusTimeline.slice(-10);
+        return thread;
+      });
+
+    })
+  };
+
+  ThreadsTimeline.prototype.refresh = function(ids) {
+  };
+
   return ThreadsTimeline;
 
 }());
