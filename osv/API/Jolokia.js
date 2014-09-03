c09ae139ed4d37f28b3526f4815d1fca778f1014
Daniel Zautner <dzautner@gmail.com>
2014-09-03 10:49:23 +0300

    Added Jolokia API layer
    Added a new class to the OSv API layer with a a method to retrieve all
    fo the MBeans in an wasy to display tree format.

---
+++ osv/API/Jolokia.js
@@ -0,0 +1,55 @@
+var OSv = OSv || {};
+OSv.API = OSv.API || {};
+
+OSv.API.Jolokia = (function() {
+
+  var apiGETCall = helpers.apiGETCall,
+    MBeans = {};
+
+
+  MBeans.tree = function() {
+
+    return apiGETCall("/jolokia/list")().then(function (res) {
+      
+      var MBeansObjects = $.map(res.value, function (MBeansObject, MBeansObjectName) {
+        
+        var parsed = {
+          name: MBeansObjectName,
+          types: {}
+        };
+
+        $.map(MBeansObject, function (value, key) {
+          var typeName = key.match(/type=(.*?)($|,)/)[1];
+          var name = key.match(/name=(.*?)($|,)/);
+          if (!parsed.types[typeName]) {
+            var type = {}
+            type.objects = [];
+            type.attr = value.attr;
+            parsed.types[typeName] = type;
+          } 
+          if (name) {
+            var object = {
+              name: name[1],
+              attr: value.attr
+            }
+            parsed.types[typeName].objects.push(object);
+          }
+        });
+
+        parsed.types = $.map(parsed.types, function (value, name) {
+          value.name = name;
+          return value;
+        });
+        return parsed;
+      });
+
+      return MBeansObjects;
+    });
+  };
+
+
+  return { 
+    MBeans: MBeans
+  };
+
+}());
