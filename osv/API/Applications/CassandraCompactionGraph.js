fb45f2baecb48a2bbc2c8ac570fdfd5f86fb48e3
Daniel Zautner <dzautner@gmail.com>
2014-09-04 14:55:41 +0300

    added cassantra compaction graph api

---
+++ osv/API/Applications/CassandraCompactionGraph.js
@@ -0,0 +1,48 @@
+var OSv = OSv || {};
+OSv.API = OSv.API || {};
+OSv.API.Applications = OSv.API.Applications || {};
+OSv.API.Applications.CassandraCompactionGraph = (function() {
+
+  var Jolokia = OSv.API.Jolokia,
+    apiGETCall = helpers.apiGETCall
+
+
+  function CassandraCompactionGraph() {
+    this.startPulling();
+  }
+
+  CassandraCompactionGraph.prototype.bytesCompacted = [];
+  
+  CassandraCompactionGraph.prototype.bytesTotalInProgress = [];
+
+  CassandraCompactionGraph.prototype.pullData = function () {
+    var self = this;
+    $.when(
+      Jolokia.read("org.apache.cassandra.metrics:name=BytesCompacted,type=Compaction"),
+      Jolokia.read("org.apache.cassandra.metrics:name=BytesTotalInProgress,type=Compaction")
+    ).then(function (bytesCompacted, bytesTotalInProgress) {
+      var timestamp = Date.now();
+      self.bytesCompacted.push([timestamp, bytesCompacted])
+      self.bytesTotalInProgress.push([timestamp, bytesTotalInProgress])
+    })
+  };
+
+  CassandraCompactionGraph.prototype.safePlot = function (plot) {
+    return (plot.length > 0 ? plot : [[]]).slice(-9);
+  };
+
+  CassandraCompactionGraph.prototype.getData = function() {
+    return [
+      this.safePlot(this.bytesCompacted),
+      this.safePlot(this.bytesTotalInProgress)
+    ]
+  }
+  
+  CassandraCompactionGraph.prototype.startPulling = function () {
+    this.interval = setInterval(this.pullData.bind(this), 2000);
+  };
+
+  var singleton = new CassandraCompactionGraph();
+  
+  return singleton;
+}());
