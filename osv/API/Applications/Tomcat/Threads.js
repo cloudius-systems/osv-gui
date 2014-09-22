4db4c06da80c50eaa036755b773c2b11ef14f0b7
Lord Daniel Zautner <dzautner@gmail.com>
2014-09-22 16:30:58 +0300

    added api to retreive tomcat threads graphs

--- osv/API/Applications/Tomcat/Threads.js
+++ osv/API/Applications/Tomcat/Threads.js
@@ -16,6 +16,47 @@ OSv.API.Applications.Tomcat.Threads = (function() {
     });
   }
 
+  Threads.prototype = Object.create(CassandraGraph.prototype);
+
+  Threads.prototype.plots = null;
+
+  Threads.prototype.pullData = function () {
+    var self = this;
+    if (window.globalPause) return;
+    Jolokia.read("Catalina:type=ThreadPool,name=*?ignoreErrors=true")
+    .then(function (res) {
+      if (self.plots == null) self.plots = {};
+      console.log("Got data for Tomcat/Threads graph, current data: ", self.plots)
+      var timestamp = res.timestamp * 1000;
+      $.map(res.value, function (value, key) {
+        var name = value.name, //key.match(/name=(.*?)($|,)/)[1],
+          threadsBusy = value.currentThreadsBusy,
+          threadsBusyLabel = name + " - Busy Threads",
+          threadsCount = value.currentThreadCount,
+          threadsCountLabel = name + " - Threads Count";
+
+        if (!self.plots[threadsBusyLabel]) self.plots[threadsBusyLabel] = [];
+        if (!self.plots[threadsCountLabel]) self.plots[threadsCountLabel] = [];
+        self.plots[threadsBusyLabel].push([ timestamp, threadsBusy])
+        self.plots[threadsCountLabel].push([ timestamp, threadsCount])
+      });
+    })
+  };
+
+  Threads.prototype.getPlots = function () {
+    if (this.plots == null) return [[null]];
+    return $.map(this.plots, function (plot) {
+      return [plot.slice(-9)];
+    });
+  };
+
+  Threads.prototype.getLabels = function () {
+    if (this.plots == null) return [];
+    return $.map(this.plots, function (plot, name) {
+      return name;
+    });
+  };
+
   var singleton = new Threads();
   
   return singleton;
