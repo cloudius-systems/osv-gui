67ccc4d5cea5b838e1c2b0a0ea08703b1551061d
Lord Daniel Zautner <dzautner@gmail.com>
2014-09-24 14:50:06 +0300

    Claned up batch requests class

--- osv/API/BatchRequest.js
+++ osv/API/BatchRequest.js
@@ -8,6 +8,7 @@ function BatchRequests (basePath, batchAPIPath) {
   this.interval = setInterval(this.commitRequestsQueue.bind(this), 2000)
 };
 
+<<<<<<< HEAD
 BatchRequests.prototype.addRequest = function (request) {
   var promise = $.Deferred();
   this.queue.push(request);
@@ -20,6 +21,27 @@ BatchRequests.prototype.getFormatedQueue = function () {
     return {"method": request.method, "relative_url": request.path}
   });
 };
+=======
+  function BatchRequests (basePath, batchAPIPath) {
+    this.url = basePath + batchAPIPath;
+    this.interval = setInterval(this.commitRequestsQueue.bind(this), 2000)
+  };
+
+  BatchRequests.prototype.queue = [];
+
+  BatchRequests.prototype.promises = [];
+
+  BatchRequests.prototype.clearInterval = function () {
+    clearInterval(this.interval);
+  };
+
+  BatchRequests.prototype.addRequest = function (request) {
+    var promise = $.Deferred();
+    this.queue.push(request);
+    this.promises.push(promise);
+    return promise;
+  };
+>>>>>>> Claned up batch requests class
 
 BatchRequests.prototype.resetQueue = function () {
   this.queue = [];  
@@ -37,6 +59,7 @@ BatchRequests.prototype.resolve = function (promises, responses) {
   });
 };
 
+<<<<<<< HEAD
 BatchRequests.prototype.reject = function (promises, error) {
   promises.forEach(function (promise) {
     promise.reject(err)
@@ -87,4 +110,80 @@ BatchRequests.prototype.get = function (path) {
 var singleton = new BatchRequests(Settings.BasePath, "/api/batch")
 
 module.exports = singleton;
+=======
+  BatchRequests.prototype.parseResponses = function (responses) {
+    return JSON.parse(responses.replace(/<h1>We didn't find the page you were looking for<\/h1>/g, 404))
+  };
+
+  BatchRequests.prototype.resolve = function (promises, responses) {
+    this.parseResponses(responses).forEach(function (response, idx) {
+      if (response.code == 200) {
+        promises[idx].resolve(response.body);
+      } else {
+        promises[idx].reject(response.code)        
+      }
+    });
+  };
+
+  BatchRequests.prototype.reject = function (promises, error) {
+    promises.forEach(function (promise) {
+      promise.reject(error)
+    });
+  };
+
+  BatchRequests.prototype.getRequests = function () {
+    return JSON.stringify(this.queue.map(function (request) {
+        return {"method":"GET", "relative_url":request.path}
+    }))
+  };
+
+  BatchRequests.prototype.getFormData = function () {
+    var formData = new FormData(),
+        requests = this.getRequests();
+
+    formData.append("batch", requests);
+    return formData;
+  };
+
+  BatchRequests.prototype.postData = function (formData) {
+    return $.ajax({
+      url: this.url,
+      data: formData,
+      cache: false,
+      contentType: false,
+      processData: false,
+      type: 'POST',
+    });
+  };
+
+  BatchRequests.prototype.commitRequestsQueue = function () {
+    var self = this,
+        queue = this.queue,
+        promises = this.promises,
+        formData = this.getFormData();
+      
+    if (queue.length == 0) return;
+    this.resetQueue();
+    this.postData(formData)
+    .then(function (responses) {
+      self.resolve(promises, responses);
+    })
+    .fail(function (error) {
+      self.reject(promises, error)  
+    });
+  };
+
+  BatchRequests.prototype.get = function (path) {
+    return this.addRequest({
+      method: "GET",
+      path: path
+    });
+  };
+
+  var singleton = new BatchRequests(OSv.Settings.BasePath, "/api/batch");
+  
+  return {
+    get: singleton.get.bind(singleton)
+  }
+>>>>>>> Claned up batch requests class
 
