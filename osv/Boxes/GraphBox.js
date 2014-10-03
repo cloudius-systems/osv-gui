5b9228c92ce711ac70528293269613d1a0a8c33e
Lord Daniel Zautner <dzautner@gmail.com>
2014-10-03 13:04:19 +0300

    Changed graphs default design
    The graphs visualization didn't much the amount of points
    displayed on them, making it look horrible.
    I changed the default design to match better the one from the mockups.

--- osv/Boxes/GraphBox.js
+++ osv/Boxes/GraphBox.js
@@ -14,6 +14,24 @@ GraphBox.prototype.subscribe = function () {
 }
 GraphBox.prototype.baseSettings = function() {
   return {
+    grid: {
+        drawGridLines: true,        // wether to draw lines across the grid or not.
+        gridLineColor: '#CCCCCC',    // *Color of the grid lines.
+        background: '#FDFDFD',      // CSS color spec for background color of grid.
+        borderColor: '#CCCCCC',     // CSS color spec for border around grid.
+        borderWidth: 0.1,           // pixel width of border around grid.
+        shadow: false,               // draw a shadow for grid.
+        renderer: $.jqplot.CanvasGridRenderer,  // renderer to use to draw the grid.
+        rendererOptions: {}         // options to pass to the renderer.  Note, the default
+                                    // CanvasGridRenderer takes no additional options.
+    },
+    seriesDefaults: {
+      lineWidth: 0.5,
+      markerOptions: {
+        show: false,
+        size: 0.2
+      },
+    },
     highlighter: {
           show: true,
           sizeAdjust: 7.5
