28412bae13bd691fb55f896e3111d3d8d0b88946
Lord Daniel Zautner <dzautner@gmail.com>
2014-10-03 17:40:36 +0300

    changed colours of disk usage chart to match others:

--- osv/Boxes/DiskUsageBox.js
+++ osv/Boxes/DiskUsageBox.js
@@ -23,6 +23,19 @@ DiskUsageBox.prototype.postRender = function() {
         ]
       ], 
       { 
+        seriesColors: [ "#00cc33", "#2553da" ],
+
+         grid: {
+            drawGridLines: true,        // wether to draw lines across the grid or not.
+            gridLineColor: '#CCCCCC',    // *Color of the grid lines.
+            background: '#FFF',      // CSS color spec for background color of grid.
+            borderColor: '#CCCCCC',     // CSS color spec for border around grid.
+            borderWidth: 0.1,           // pixel width of border around grid.
+            shadow: false,               // draw a shadow for grid.
+            renderer: $.jqplot.CanvasGridRenderer,  // renderer to use to draw the grid.
+            rendererOptions: {}         // options to pass to the renderer.  Note, the default
+                                        // CanvasGridRenderer takes no additional options.
+        },
         seriesDefaults: {
           renderer: jQuery.jqplot.PieRenderer, 
           rendererOptions: {
