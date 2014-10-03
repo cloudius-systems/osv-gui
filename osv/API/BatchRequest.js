60bfc0ae35a8372404aa6ded7c7a540a757d4416
Lord Daniel Zautner <dzautner@gmail.com>
2014-10-03 12:11:06 +0300

    returned methods removed by accident on merge

--- osv/API/BatchRequest.js
+++ osv/API/BatchRequest.js
@@ -43,46 +43,74 @@ BatchRequests.prototype.reject = function (promises, error) {
   });
 };
 
-BatchRequests.prototype.getFormData = function () {
-  var formData = new FormData(),
-    requests = JSON.stringify(this.queue.map(function (request) {
-      return {"method":"GET", "relative_url":request.path}
-    }));
-  formData.append("batch", requests);
-  return formData;
+BatchRequests.prototype.getRequests = function () {   
+  return JSON.stringify(this.queue.map(function (request) {    
+      return {"method":"GET", "relative_url":request.path}   
+  }))    
 };
 
-BatchRequests.prototype.commitRequestsQueue = function () {
-  var self = this,
-      queue = this.queue,
-      promises = this.promises,
-      formData = this.getFormData();
-    
-  if (queue.length == 0) return;
-  this.resetQueue();
-
-  $.ajax({
-    url: this.url,
-    data: formData,
-    cache: false,
-    contentType: false,
-    processData: false,
-    type: 'POST',
-  })
-  .then(function (responses) {
-    self.resolve(promises, responses);
-  })
-  .fail(function (error) {
-    self.reject(promises, error)  
-  });
-};
-
-BatchRequests.prototype.get = function (path) {
-  return this.addRequest({
-    methd: "GET",
-    path: path
-  });
-};
+BatchRequests.prototype.getFormData = function () {    
+  var formData = new FormData(),   
+      requests = this.getRequests();   
+ 
+  formData.append("batch", requests);    
+  return formData;   
+};   
+ 
+BatchRequests.prototype.postData = function (formData) {   
+  return $.ajax({    
+    url: this.url,   
+    data: formData,    
+    cache: false,    
+    contentType: false,    
+    processData: false,    
+    type: 'POST',    
+  });    
+};   
+ 
+BatchRequests.prototype.commitRequestsQueue = function () {    
+  var self = this,   
+      queue = this.queue,    
+      promises = this.promises,    
+      formData = this.getFormData();   
+       
+  if (queue.length == 0) return;   
+  this.resetQueue();   
+  this.postData(formData)    
+  .then(function (responses) {   
+    if (window.globalPause) {    
+      $(document).on("play", function () {   
+        self.resolve(promises, responses);   
+      })   
+    } else {   
+        self.resolve(promises, responses);   
+    }    
+  })   
+  .fail(function (error) {   
+    self.reject(promises, error)     
+  });    
+};   
+ 
+BatchRequests.prototype.ajax = function (method, path) {   
+  return this.addRequest({   
+    method: method,    
+    path: path   
+  });    
+}    
+BatchRequests.prototype.get = function (path) {    
+  return this.ajax("GET", path);   
+};   
+ 
+BatchRequests.prototype.post = function (path, data) {   
+  data = "?" + $.map(data, function (value, key) {   
+    return key + "=" + value;    
+  }).join("&")   
+  return this.ajax("POST", path + data);   
+};   
+ 
+BatchRequests.prototype.delete = function (path) {   
+  return this.ajax("DELETE", path);    
+};   
 
 var singleton = new BatchRequests(Settings.BasePath, "/api/batch")
 
