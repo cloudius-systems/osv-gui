787a86d8eb47275d273567feefda882767fc09de
Lord Daniel Zautner <dzautner@gmail.com>
2014-10-10 12:03:27 +0300

    added iframe insertion in swagger ui handler

--- osv/PageHandlers/Swagger.js
+++ osv/PageHandlers/Swagger.js
@@ -1,7 +1,8 @@
 function Swagger() {}
 
 Swagger.prototype.handler = function () {
-  console.log('everyday Im swaggering');  
+  var $iframe = $("<iframe style='border:none' id='osvContainer' src='/dashboard_static/swagger/'>")
+  $("#main").append($iframe);
 };
 
 module.exports = Swagger;
